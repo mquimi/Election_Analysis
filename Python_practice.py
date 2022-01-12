7903314aaf40bc2db6ca21bd9bae82f7b7ee1127
Pace <pace@michelles-mbp-2.myfiosgateway.com>
2022-01-12 00:06:23 -0500

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,156 @@
+#print("Hello World")
+
+#counties = ["Arapahoe","Denver","Jefferson"]
+#if counties[1] == 'Denver':
+#  print(counties[1])
+    
+
+#What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')
+
+
+
+# What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
+
+
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+
+
+
+    
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+percentage_votes = (my_votes / total_votes) * 100
+print("I received " + str(percentage_votes)+"% of the total votes.")
+
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+
+#Multiline F-Strings
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
+
+
+#print("Hello World")
+
+#counties = ["Arapahoe","Denver","Jefferson"]
+#if counties[1] == 'Denver':
+#  print(counties[1])
+    
+
+#What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')
+
+
+
+# What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
+
+
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+
+    
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+percentage_votes = (my_votes / total_votes) * 100
+print("I received " + str(percentage_votes)+"% of the total votes.")
+
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+#message_to_candidate = (
+ #   f"You received {candidate_votes} number of votes. "
+  #  f"The total number of votes in the election was {total_votes}. "
+   # f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+print(message_to_candidate)
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+county = str(counties_dict.keys)
+voters = int(counties_dict.values)
+msg=(f"{county} county has {voters:,} registered voters.")
+print(msg)
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Election_Analysis
-using Python
+# Stock_Analysis with VBA
+
+
+### 1. Overview of Project: Explain the purpose of this analysis.
+
+While working on this project, I was able to incoporate my excel skills, along with my new VBA coding skills. The purpose of this project was to help Steve create a report that will help analyze new stocks for his parents to invest. While running the yearly stock reports of 2017 and 2018, we are also coding the time it takes to loop through all the data once. Then we will be comparing the refactored code to the original code to determine which analysis took longer.
+
+### 2. Results: Using images and examples of your code, compare the stock performance between 2017 and 2018, as well as the execution times of the original script and the refactored script.
+For the original code, we were using coding that included nested looping. The nested loop coding seem to have taken longer than the code with individualizing each loop. For example, Image A (original) and C shows the Green stock 2017 and 2018 report time and Image B and D shows the VBA Challenge 2017 report time. 
+
+![alt text](https://github.com/mquimi/Stock_Analysis/blob/main/Resources/VBA_Challenge_2017_GS.png)
+![alt text](https://github.com/mquimi/Stock_Analysis/blob/main/Resources/VBA_Challenge_2017.png)
+
+![alt text](https://github.com/mquimi/Stock_Analysis/blob/main/Resources/VBA_Challenge_2018_GS.png)
+![alt text](https://github.com/mquimi/Stock_Analysis/blob/main/Resources/VBA_Challenge_2018.png)
+
+
+### 3. Summary: In a summary statement, address the following questions.
+- What are the advantages or disadvantages of refactoring code?
+
+- How do these pros and cons apply to refactoring the original VBA script?
